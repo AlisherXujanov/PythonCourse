f618923c3ae140cb3b7161631828e2b20c26e872
Alisher <alisherxujanov163@gmail.com>
2024-07-04 12:56:07 +0500

    Amir|Amirbek

--- Online-lesson/Amir.py
+++ Online-lesson/Amir.py
@@ -1,4 +1,11 @@
 
-def какой_больше(,):
-    if 
+def get_max(a,b,c) -> int:
+    return a if a > b and a > c else (b if b > c and b > a else c)
+
+
+a = 1
+b = 2
+c = 3
+r = get_max(a,b,c)
+print(r)
 
--- Online-lesson/main.py
+++ Online-lesson/main.py
@@ -1,10 +1,10 @@
-def get_max(a, b) -> int:
+
+def get_max(a, b, c,) -> int:
     return a if a > b and a > c else (b if b > c and b > a else c)
 
-    
+
 a = 1
 b = 2
-c = 3 
+c = 3
 r = get_max(a, b, c)
 print(r)
-
